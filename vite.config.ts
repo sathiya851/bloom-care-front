9f60e55b4233f8b7393cc4db7dc14c676aef06a6
Sathya <sathiyaveerappan@gmail.com>
2026-03-24 17:48:06 +0530

    typo build fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/bloom-care-font/',
+  base: '/bloom-care-front/',
 
   server: {
     host: "::",
